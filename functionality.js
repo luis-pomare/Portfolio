8179ef7e1ab21459bec7265b1f17f830e3d63ef4
unknown <luispomare92@gmail.com>
2022-01-25 14:36:31 -0500

    Fix linter errors

--- functionality.js
+++ functionality.js
@@ -1,26 +1,26 @@
 // Create variables
-let menu = document.querySelector("nav");
-let icon = document.querySelector(".menu");
+const menu = document.querySelector('nav');
+const icon = document.querySelector('.menu');
 
-//Event listener calling
-menu.addEventListener("click", whenClicked);
+function addMenuItems() {
+  menu.classList.add('showMenu');
+  document.getElementById('icon-container').classList.remove('menu-invisible');
+  icon.src = 'images/close-movile-menu-Icon.svg';
+}
+
+function removeMenuItems() {
+  menu.classList.remove('showMenu');
+  document.getElementById('icon-container').classList.add('menu-invisible');
+  icon.src = 'images/menu.svg';
+}
 
 function whenClicked() {
-  if (menu.classList.contains("showMenu")) {
+  if (menu.classList.contains('showMenu')) {
     removeMenuItems();
   } else {
     addMenuItems();
   }
 }
 
-function addMenuItems() {
-  menu.classList.add("showMenu");
-  document.getElementById("icon-container").classList.remove("menu-invisible");
-  icon.src = "images/close-movile-menu-Icon.svg";
-}
-
-function removeMenuItems() {
-  menu.classList.remove("showMenu");
-  document.getElementById("icon-container").classList.add("menu-invisible");
-  icon.src = "images/menu.svg";
-}
+// Event listener calling
+menu.addEventListener('click', whenClicked);
