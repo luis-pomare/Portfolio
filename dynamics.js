99ead50f23230e4dc1dd11f9470dfc08f434cfa2
unknown <luispomare92@gmail.com>
2022-02-01 15:57:52 -0500

    Fix linter errors

--- dynamics.js
+++ dynamics.js
@@ -2,31 +2,31 @@
 //  Note: All the styles applied to dynamic elements are at bottom of styles.css
 
 //  Extract hamburguer menu icon object from DOM
-const menuIcon = document.getElementById("menu-icon");
+const menuIcon = document.getElementById('menu-icon');
 menuIcon.tabIndex = 1;
 
 //  Create menu container
-let dynamicNav = document.createElement("nav"); //Menu container
-dynamicNav.id = "dynamicNav";
+const dynamicNav = document.createElement('nav'); // Menu container
+dynamicNav.id = 'dynamicNav';
 
 //  Create closer icon
-let closerIcon = document.createElement("img");
-closerIcon.id = "closerIcon";
-closerIcon.src = "images/close-mobile-menu-Icon.svg";
+const closerIcon = document.createElement('img');
+closerIcon.id = 'closerIcon';
+closerIcon.src = 'images/close-mobile-menu-Icon.svg';
 
 //  Create Links to the sections of the page
-let firstMenuElement = document.createElement("a");
-let secondMenuElement = document.createElement("a");
-let thirdMenuElement = document.createElement("a");
-firstMenuElement.id = "firstMenuElement";
-secondMenuElement.id = "secondMenuElement";
-thirdMenuElement.id = "thirdMenuElement";
-firstMenuElement.href = "#portfolio";
-secondMenuElement.href = "#about-tittle";
-thirdMenuElement.href = "#contact-text";
-firstMenuElement.innerText = "Portfolio";
-secondMenuElement.innerText = "About";
-thirdMenuElement.innerText = "Contact";
+const firstMenuElement = document.createElement('a');
+const secondMenuElement = document.createElement('a');
+const thirdMenuElement = document.createElement('a');
+firstMenuElement.id = 'firstMenuElement';
+secondMenuElement.id = 'secondMenuElement';
+thirdMenuElement.id = 'thirdMenuElement';
+firstMenuElement.href = '#portfolio';
+secondMenuElement.href = '#about-tittle';
+thirdMenuElement.href = '#contact-text';
+firstMenuElement.innerText = 'Portfolio';
+secondMenuElement.innerText = 'About';
+thirdMenuElement.innerText = 'Contact';
 
 //  Apend menu elements to nav container
 dynamicNav.appendChild(closerIcon);
@@ -43,8 +43,8 @@ function deactiveMenu() {
   dynamicNav.remove();
 }
 
-menuIcon.addEventListener("click", activeMenu);
-closerIcon.addEventListener("click", deactiveMenu);
-firstMenuElement.addEventListener("click", deactiveMenu);
-secondMenuElement.addEventListener("click", deactiveMenu);
-thirdMenuElement.addEventListener("click", deactiveMenu);
+menuIcon.addEventListener('click', activeMenu);
+closerIcon.addEventListener('click', deactiveMenu);
+firstMenuElement.addEventListener('click', deactiveMenu);
+secondMenuElement.addEventListener('click', deactiveMenu);
+thirdMenuElement.addEventListener('click', deactiveMenu);
